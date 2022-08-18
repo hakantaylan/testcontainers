2da4750eff56400c63601805ab4c2e4875537d59
hakantaylan <hakantaylan@users.noreply.github.com>
2022-08-18 14:06:34 +0300

    docker compose example added

---
+++ testcontainers-docker/src/main/java/dev/fullstackcode/tc/docker/controller/DepartmentController.java
@@ -0,0 +1,29 @@
+package dev.fullstackcode.tc.docker.controller;
+
+import dev.fullstackcode.tc.docker.entity.Department;
+import dev.fullstackcode.tc.docker.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/department")
+public class DepartmentController {
+
+    @Autowired
+    DepartmentService departmentService;
+
+    @GetMapping(value="/{id}")
+    public Department getDepartmentById(@PathVariable Integer id) {
+        return departmentService.getDepartmentById(id);
+    }
+
+    @GetMapping
+    public List<Department> getDepartments() {
+        return departmentService.getAllDepartments();
+    }
+}
---
+++ testcontainers-docker/src/test/java/dev/fullstackcode/tc/docker/it/BaseIT2.java
@@ -0,0 +1,45 @@
+package dev.fullstackcode.tc.docker.it;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.File;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@DirtiesContext
+public class BaseIT2 {
+
+    @Autowired
+    protected TestRestTemplate testRestTemplate;
+    private static int POSTGRES_PORT = 5432;
+
+    @Container
+    private static final DockerComposeContainer environment =
+            new DockerComposeContainer(new File("src/test/resources/docker-compose.yaml"))
+                    .withExposedService("postgres", POSTGRES_PORT, Wait.forListeningPort())
+                    .withLocalCompose(true);
+
+    @DynamicPropertySource
+    public static void properties(DynamicPropertyRegistry registry) {
+
+        String postgresUrl = environment.getServiceHost("postgres", POSTGRES_PORT)
+                + ":" +
+                environment.getServicePort("postgres", POSTGRES_PORT);
+
+        registry.add("spring.datasource.url", () -> "jdbc:postgresql://" + postgresUrl + "/eis");
+        registry.add("spring.datasource.username", () -> "postgres");
+        registry.add("spring.datasource.password", () -> "postgres");
+
+    }
+}
